5d86b575011cb17a6e5a0191d924a1afb2f474a2
Kwarku <p.kuklicz@gmail.com>
2017-09-20 06:54:04 +0200

    Refactoring LockerApp v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import utils.ResourceUtils;
 
 import java.io.File;
 
-public class Main extends Application{
+public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
 
@@ -16,8 +17,10 @@ public class Main extends Application{
 //        FXMLLoader loader = new FXMLLoader(Main.class.getClass().getResource("/fxml/MainView.fxml"));
 //        Scene scene = new Scene(ResourceUtils.loadFXml("/fxml/MainView.fxml"),600,600);
 
-        primaryStage.setScene(new Scene(ResourceUtils.loadFXml("/fxml/GameView.fxml"),500,600));
+        primaryStage.setScene(new Scene(ResourceUtils.loadFXml("/fxml/GameView.fxml"), 500, 600));
         primaryStage.setFullScreen(true);
+
+        primaryStage.getIcons().add(new Image("logo jpg.jpg"));
 
         primaryStage.setTitle(ResourceUtils.getResourceBundle().getString("app.title"));
         File file = new File("style.css");
